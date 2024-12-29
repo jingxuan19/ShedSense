4a008135056345042fd79da936d9ce09e3e65762
jingxuan19 <tanjingxuan19@gmail.com>
2024-12-29 23:05:17 +0000

    Created Yolo model class

--- src/detection/roi/YOLOmodel.py
+++ src/detection/roi/YOLOmodel.py
@@ -1,10 +1,15 @@
-from ultralytics import YOLOv10
+from ultralytics import YOLO
 import cv2
 import numpy as np
 
-def ROI_detection(frame):
-    model = YOLOv10()
+class YOLOmodel:
+    def __init__(self, ver: int):
+        self.ver = ver
+        self.model = YOLO("/home/shedsense1/ShedSense/src/detection/roi/yolov10n.pt")
 
-    results = model(frame)
-    
+        self.model.export(format="edgetpu")
+
+    def detect(self, frame):
+        result = self.model(frame)
+        return result[0].plot()
 
