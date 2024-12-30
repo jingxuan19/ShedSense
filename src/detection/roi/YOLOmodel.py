415fb16a7621bf4ca83dab1e710bef7d11fc48b2
jingxuan19 <tanjingxuan19@gmail.com>
2024-12-30 14:20:15 +0000

    Added functionality for Coral TPU

--- src/detection/roi/YOLOmodel.py
+++ src/detection/roi/YOLOmodel.py
@@ -1,15 +1,11 @@
 from ultralytics import YOLO
-import cv2
-import numpy as np
 
 class YOLOmodel:
     def __init__(self, ver: int):
         self.ver = ver
-        self.model = YOLO("/home/shedsense1/ShedSense/src/detection/roi/yolov10n.pt")
-
-        self.model.export(format="edgetpu")
+        self.model = YOLO("/home/shedsense1/ShedSense/src/detection/yolov10n_saved_model/yolov10n_full_integer_quant_edgetpu.tflite", task="detect")
 
     def detect(self, frame):
-        result = self.model(frame)
+        result = self.model.predict(frame)
         return result[0].plot()
 
