59aba794321be9b08b4c17e69653fb49bf40f5aa
jingxuan19 <tanjingxuan19@gmail.com>
2025-01-07 15:32:39 +0000

    Fixed counting bug

--- src/roi/detection/roi_detection.py
+++ src/roi/detection/roi_detection.py
@@ -12,14 +12,17 @@ def roi_detection(is_cpu: bool, camera):
         cv2.imshow("Camera", result[0].plot())
 
         detected_objects = Yolomodel.separate_objects(result) # box, score, c_id, c_name
-        if len(detected_objects[2]):
-            class_count = torch.bincount(detected_objects[2])
-            print(f"number of bikes: {class_count[1]}")
-            print(f"number of people: {class_count[0]}")
-        else:
+        class_count = torch.bincount(detected_objects[2])
+
+        if len(class_count) == 0:
             print(f"number of bikes: 0")
             print(f"number of people: 0")
-        
-        
+        elif len(class_count) == 1:
+            print(f"number of bikes: 0")  
+            print(f"number of people: {class_count[0]}")
+        else:
+            print(f"number of bikes: {class_count[1]}")
+            print(f"number of people: {class_count[0]}")
+
         if cv2.waitKey(1) == ord('q'):
             break
