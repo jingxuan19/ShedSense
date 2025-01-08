aa8d9e40ad3e7c1e5ec5d466e31a5d0200eb6ca1
jingxuan19 <tanjingxuan19@gmail.com>
2025-01-08 14:19:39 +0000

    partial loi implementation , primarily borders and masks

--- src/loi/detection/loi_detection.py
+++ src/loi/detection/loi_detection.py
@@ -1,24 +1,56 @@
 import cv2
-import torch
+import numpy as np
 from models.YOLOmodel import YOLOmodel
+import yaml
+from loi.detection.masking import masking
+from loi.detection.load_lines import load_lines
 
-def roi_detection(is_cpu: bool, camera):
+LOCATION = "test_1"
+WINDOW_SIZE = (720, 1720)
+
+def loi_detection(is_cpu: bool, camera):   
+    # Everything concerning the region of interest (shed) can be calculated beforehand
+    borders = load_lines(LOCATION)
+            
+    yolo_roi = masking(WINDOW_SIZE, borders)
+
+    # Yolomodel
     Yolomodel = YOLOmodel(is_cpu)
 
     while True:
-        frame = camera.capture_array("main")       
-        result = Yolomodel.detect(frame)   
+        frame = camera.capture_array("main")   
+        # print(frame.shape)
         
-        cv2.imshow("Camera", result[0].plot())
+        for line in borders:
+            # line coords must be in (x,y)
+            cv2.line(frame, line.pt1, line.pt2, color=(0, 255, 0), thickness=5)
+                
+        result = Yolomodel.detect(frame)   
+        detected_objects = Yolomodel.separate_objects(result) # boxes, score, c_id, c_name
 
-        detected_objects = Yolomodel.separate_objects(result) # box, score, c_id, c_name
-        if len(detected_objects[2]):
-            class_count = torch.bincount(detected_objects[2])
-            print(f"number of bikes: {class_count[1]}")
-            print(f"number of people: {class_count[0]}")
-        else:
-            print(f"number of bikes: 0")
-            print(f"number of people: 0")
+        for detected in detected_objects:
+            # filter bikes and people
+            if detected.class_id not in (0,1):
+                continue
+            
+            object_mask = np.zeros(WINDOW_SIZE)
+            cv2.rectangle(object_mask, detected.box[0], detected.box[1], 255, -1)
+            
+            if np.sum(cv2.bitwise_and(yolo_roi, object_mask)) != 0:
+                # there's an overlap between the object bounding box and the roi for yolo
+                #Lucas-Kanade
+                pass   
+        
+        cv2.imshow("LOI implementation", result[0].plot())
+
+       
+        # if len(detected_objects[2]):
+        #     class_count = torch.bincount(detected_objects[2])
+        #     print(f"number of bikes: {class_count[1]}")
+        #     print(f"number of people: {class_count[0]}")
+        # else:
+        #     print(f"number of bikes: 0")
+        #     print(f"number of people: 0")
         
         
         if cv2.waitKey(1) == ord('q'):
--- src/roi/detection/roi_detection.py
+++ src/roi/detection/roi_detection.py
@@ -11,14 +11,17 @@ def roi_detection(is_cpu: bool, camera):
         
         cv2.imshow("Camera", result[0].plot())
 
-        detected_objects = Yolomodel.separate_objects(result) # box, score, c_id, c_name
-        if len(detected_objects[2]):
-            class_count = torch.bincount(detected_objects[2])
-            print(f"number of bikes: {class_count[1]}")
-            print(f"number of people: {class_count[0]}")
-        else:
-            print(f"number of bikes: 0")
-            print(f"number of people: 0")
+        detected_objects = Yolomodel.separate_objects(result) # boxes, score, class_id, class_name
+        bikes = 0
+        people = 0
+        for detected in detected_objects:
+            if detected["class_id"] == 0:
+                people += 1
+            elif detected["class_id"] == 1:
+                bikes += 1
+
+        print(f"number of bikes: {bikes}")
+        print(f"number of people: {people}")
         
         
         if cv2.waitKey(1) == ord('q'):
