a3c1873fa8d195e5663588860987ed24cdcf7128
Tan Jing Xuan <tanjingxuan19@gmail.com>
2025-02-11 11:56:36 +0000

    Allowed saving of videos on server-side

--- server/src/recording_stop.py
+++ server/src/recording_stop.py
@@ -2,4 +2,8 @@ from mqtt_server import MQTTServerClient
 
 Client = MQTTServerClient()
 
-Client.publish("recording_control", "STOP")
+while True:
+    ret = Client.publish("shedsense/recording_control", "STOP")
+    if ret == 0:
+        print("Done!")
+        break
