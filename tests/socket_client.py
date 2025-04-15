d046145d43c791443598d8a523d57e8996fca6e4
Tan Jing Xuan <tanjingxuan19@gmail.com>
2025-04-15 17:45:09 +0100

    git ignore update with calibration

--- tests/socket_client.py
+++ tests/socket_client.py
@@ -4,8 +4,11 @@ import numpy as np
 import struct
 
 client_socket = socket.socket()
-client_socket.connect(("10.247.11.189", 8000))
+# client_socket.connect(("10.247.11.189", 8000))
+client_socket.connect(("192.168.0.164", 8000))
 conn = client_socket.makefile('rb')
+
+count = 0
 
 try:
     while True:
@@ -16,8 +19,13 @@ try:
         data = conn.read(size)
         frame = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
         cv2.imshow('Stream', frame)
-        if cv2.waitKey(1) == 27:  # ESC key to exit
+        key = cv2.waitKey(1)
+        if key == 27:  # ESC key to exit
             break
+        elif key == ord("s"):
+            print(f"SAVING{count}")
+            cv2.imwrite(rf"C:\Users\tanji\OneDrive\Cambridge\2\Project\ShedSense\calbration\{count}.png", frame)
+            count += 1
 finally:
     conn.close()
     client_socket.close()
